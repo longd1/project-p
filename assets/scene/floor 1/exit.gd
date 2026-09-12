dfa3fd93a1be77bae8ab0a83c1ed842cebe141d9
longd <longdinh9991@gmail.com>
2026-09-12 22:47:38 +0700

    floor1 refined1

---
+++ assets/scene/floor 1/exit.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_tree().change_scene_to_file(
+            "res://assets/scene/floor 2/floor2_completed_scene.tscn"
+		)
