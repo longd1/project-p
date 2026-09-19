b60a15c9fdbe5c5b4211070835284662bef5135c
NguyetAnh2705 <hacnguyet34@gmail.com>
2026-09-20 01:22:34 +0700

    hud node, code

--- assets/UI/HUD/hud.gd
+++ assets/UI/HUD/hud.gd
@@ -1,10 +1,27 @@
-extends Control
-@onready var hp_bar = $playerStatus/HPBar
-@onready var hp_text = $playerStatus/HPText
-func update_player_status(player):
-	var hp = player.get_health()
-	var max_hp = player.get_max_health()
+extends CanvasLayer
 
-	hp_bar.max_value = max_hp
-	hp_bar.value = hp
-	hp_text.text = str(hp) + " / " + str(max_hp)
+var base_healthbar_width = 268.0
+var base_max_health = 100.0
+
+@onready var player = $"../John"
+@onready var hp_background = $hp_background
+@onready var hp_bar = $hp_bar
+
+
+func _process(delta: float) -> void:
+	if is_instance_valid(player):
+		var max_health_ratio = float(player.get_max_health()) / base_max_health
+		var background_width = base_healthbar_width * max_health_ratio
+
+		hp_background.size.x = background_width
+
+		var health_ratio = float(player.get_health()) / float(player.get_max_health())
+		health_ratio = clamp(health_ratio, 0.0, 1.0)
+
+		var current_health_width = background_width * health_ratio
+
+		hp_bar.size.x = lerp(
+			hp_bar.size.x,
+			current_health_width,
+			10.0 * delta
+		)
